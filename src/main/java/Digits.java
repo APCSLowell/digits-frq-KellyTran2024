192247e9de82cc82a0ae2417da3c9f90983616f4
KellyTran2024 <ketran4@s.sfusd.edu>
2025-04-09 23:31:41 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,12 +7,23 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	 digitList = new ArrayList<Integer>();
+ 	   digitList.add(0, new Integer(num % 10));
+
+    int remainder = num / 10;
+    while(remainder > 0)
+    {
+        digitList.add(0, new Integer(remainder % 10));
+        remainder /= 10;
+    }    
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
+	 for(int i = 1; i < digitList.size(); i++)
+        if(digitList.get(i - 1).compareTo(digitList.get(i)) >= 0)
+            return false;
+	return true;	
 
 	}
 	
